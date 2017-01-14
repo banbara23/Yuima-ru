41a3b38f2a3331572d8a4c5aec2b15bfbed7448b
池村 和剛 <kazutakai@tt-com.net>
2017-01-15 00:23:50 +0900

    メンバー登録OK

--- public/js/app.js
+++ public/js/app.js
@@ -5,37 +5,49 @@ var config = {
     databaseURL: "https://yuima-ru.firebaseio.com",
     storageBucket: "yuima-ru.appspot.com",
     messagingSenderId: "496769769191"
-};  
+};
 firebase.initializeApp(config);
-console.log("1")
-//member
-// function writeMemberData(memberId, name, comment, imageUrl) {
-//     firebase.database().ref('members/' + memberId).set({
-//         name: name,
-//         comment: comment,
-//         profile_picture: imageUrl
-//     });
-// }
-// writeMemberData(3, "鈴木さん", "おっちゃん", "");
-console.log("2")
+
 var db = firebase.database()
-console.log("3")
+
 var vm = new Vue({
-  el: '#member-list',
-  firebase: {
-    // simple syntax, bind as an array by default
-    anArray: db.ref('members')
-    // can also bind to a query
-    // anArray: db.ref('url/to/my/collection').limitToLast(25)
-    // full syntax
-    // anObject: {
-    //   source: db.ref('url/to/my/object'),
-    //   // オプションでオブジェクトとしてバインドする
-    //   asObject: true,
-    //   // オプションでcancelCallbackを指定する
-    //   cancelCallback: function () {
-    //       //読み書きが失敗した時に呼ばれる
-    //   }
-    // }
-  }
+    el: '#member-list',
+    firebase: {
+        // simple syntax, bind as an array by default
+        anArray: db.ref('members')
+        // can also bind to a query
+        // anArray: db.ref('url/to/my/collection').limitToLast(25)
+        // full syntax
+        // anObject: {
+        //   source: db.ref('url/to/my/object'),
+        //   // オプションでオブジェクトとしてバインドする
+        //   asObject: true,
+        //   // オプションでcancelCallbackを指定する
+        //   cancelCallback: function () {
+        //       //読み書きが失敗した時に呼ばれる
+        //   }
+        // }
+    }
+})
+
+// メンバー登録
+var vueClick = new Vue({
+    el: '#insert',
+    data: {
+        name: '',
+        imageUrl: '',
+        comment: ''
+    },
+    firebase: {
+        members: db.ref('members')
+    },
+    methods: {
+        addItem: function() {
+            this.$firebaseRefs.members.push({
+                name: this.name,
+                imageUrl: this.imageUrl,
+                comment: this.comment
+            })
+        }
+    }
 })
