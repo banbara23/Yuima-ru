80a16fb49c5da7070eec5bcba204f2b53ac71c35
池村 和剛 <kazutakai@tt-com.net>
2017-01-14 17:27:21 +0900

    たくさん配置して色々修正

---
+++ public/js/app.js
@@ -0,0 +1,40 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA0guptgPg4DYx7897UCNRZj9ltDBjLCw8",
+    authDomain: "yuima-ru.firebaseapp.com",
+    databaseURL: "https://yuima-ru.firebaseio.com",
+    storageBucket: "yuima-ru.appspot.com",
+    messagingSenderId: "496769769191"
+};
+firebase.initializeApp(config);
+
+//member
+function writeMemberData(memberId, name, comment, imageUrl) {
+    firebase.database().ref('members/' + memberId).set({
+        name: name,
+        comment: comment,
+        profile_picture: imageUrl
+    });
+}
+// writeMemberData(3, "鈴木さん", "おっちゃん", "");
+var db = firebase.database()
+
+var vm = new Vue({
+  el: '#member-list',
+  firebase: {
+    // simple syntax, bind as an array by default
+    anArray: db.ref('members'),
+    // can also bind to a query
+    // anArray: db.ref('url/to/my/collection').limitToLast(25)
+    // full syntax
+    // anObject: {
+    //   source: db.ref('url/to/my/object'),
+    //   // オプションでオブジェクトとしてバインドする
+    //   asObject: true,
+    //   // オプションでcancelCallbackを指定する
+    //   cancelCallback: function () {
+    //       //読み書きが失敗した時に呼ばれる
+    //   }
+    // }
+  }
+})
