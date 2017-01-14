b694a57b57564ff208bcca6d70d40508c4b54c61
池村 和剛 <kazutakai@tt-com.net>
2017-01-14 22:53:26 +0900

    memberがfirebase hosting先でも表示された

--- public/js/app.js
+++ public/js/app.js
@@ -5,25 +5,26 @@ var config = {
     databaseURL: "https://yuima-ru.firebaseio.com",
     storageBucket: "yuima-ru.appspot.com",
     messagingSenderId: "496769769191"
-};
+};  
 firebase.initializeApp(config);
-
+console.log("1")
 //member
-function writeMemberData(memberId, name, comment, imageUrl) {
-    firebase.database().ref('members/' + memberId).set({
-        name: name,
-        comment: comment,
-        profile_picture: imageUrl
-    });
-}
+// function writeMemberData(memberId, name, comment, imageUrl) {
+//     firebase.database().ref('members/' + memberId).set({
+//         name: name,
+//         comment: comment,
+//         profile_picture: imageUrl
+//     });
+// }
 // writeMemberData(3, "鈴木さん", "おっちゃん", "");
+console.log("2")
 var db = firebase.database()
-
+console.log("3")
 var vm = new Vue({
   el: '#member-list',
   firebase: {
     // simple syntax, bind as an array by default
-    anArray: db.ref('members'),
+    anArray: db.ref('members')
     // can also bind to a query
     // anArray: db.ref('url/to/my/collection').limitToLast(25)
     // full syntax
