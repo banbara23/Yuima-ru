a652a2468e07082e198b5e44a229564aa759f806
WC-DEVSERVER5\IkemuraK <IkemuraK@kogokojupiterjcomco.onmicrosoft.com>
2017-02-06 18:25:44 +0900

    スケジュール詳細html, jsを作成

---
+++ public/js/schedule_detail.js
@@ -0,0 +1,47 @@
+// urlからパラメータを取得
+var urlParam = location.search.substring(1);
+var param = urlParam.split('&');
+var params = [];
+for (i = 0; i < param.length; i++) {
+  var paramItem = param[i].split('=');
+  params[paramItem[0]] = paramItem[1];
+}
+
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA0guptgPg4DYx7897UCNRZj9ltDBjLCw8",
+  authDomain: "yuima-ru.firebaseapp.com",
+  databaseURL: "https://yuima-ru.firebaseio.com",
+  storageBucket: "yuima-ru.appspot.com",
+  messagingSenderId: "496769769191"
+};
+firebase.initializeApp(config);
+var db = firebase.database()
+
+//firebaseに接続して予定一覧を取得し、aタグ用のlinkを作る
+// var eventList = new Array();
+// db.ref('events/' + params.id)
+//   .on('value', function(snapshot) {
+//     var id = params.id;
+//     var value = snapshot.val();
+//     value
+
+//   });
+
+new Vue({
+  el: '#schedule',
+  data: {
+    date: '???',
+    value: 'value',
+    comment: 'comment'
+  },
+  firebase: {
+    anObject: {
+      source: db.ref('events/' + params.id),
+      cancelCallback: function() {
+        console.log('firebase get error')
+      }
+    }
+  }
+})
