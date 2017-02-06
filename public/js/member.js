f16783c16bf59ab37cab8cf8ef67b904dead7297
WC-DEVSERVER5\IkemuraK <IkemuraK@kogokojupiterjcomco.onmicrosoft.com>
2017-02-06 14:00:29 +0900

    メンバー一覧のaタグにパラメータ付きになるよう修正

--- public/js/member.js
+++ public/js/member.js
@@ -1,53 +1,61 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyA0guptgPg4DYx7897UCNRZj9ltDBjLCw8",
-    authDomain: "yuima-ru.firebaseapp.com",
-    databaseURL: "https://yuima-ru.firebaseio.com",
-    storageBucket: "yuima-ru.appspot.com",
-    messagingSenderId: "496769769191"
+  apiKey: "AIzaSyA0guptgPg4DYx7897UCNRZj9ltDBjLCw8",
+  authDomain: "yuima-ru.firebaseapp.com",
+  databaseURL: "https://yuima-ru.firebaseio.com",
+  storageBucket: "yuima-ru.appspot.com",
+  messagingSenderId: "496769769191"
 };
 firebase.initializeApp(config);
-
 var db = firebase.database()
 
+var members = new Array();
+db.ref('members')
+  .on('value', function(snapshot) {
+    snapshot.forEach(function(child) {
+      var val = child.val();
+      var event = {
+        date: val.date,
+        comment: val.comment,
+        title: val.title,
+        link: 'event.html?id=' + child.key
+      }
+      var member = {
+        key: child.key,
+        name: val.name,
+        comment: val.comment,
+        link: 'member.html?key=' + child.key
+      }
+      members.push(member);
+    })
+    console.log(members);
+  });
+
 var vm = new Vue({
-    el: '#member-list',
-    firebase: {
-        // simple syntax, bind as an array by default
-        anArray: db.ref('members')
-        // can also bind to a query
-        // anArray: db.ref('url/to/my/collection').limitToLast(25)
-        // full syntax
-        // anObject: {
-        //   source: db.ref('url/to/my/object'),
-        //   // オプションでオブジェクトとしてバインドする
-        //   asObject: true,
-        //   // オプションでcancelCallbackを指定する
-        //   cancelCallback: function () {
-        //       //読み書きが失敗した時に呼ばれる
-        //   }
-        // }
-    }
+  el: '#member-list',
+  data: {
+    anArray: members
+  }
 })
 
 // メンバー登録
 var vueClick = new Vue({
-    el: '#member-insert',
-    data: {
-        name: '',
-        imageUrl: '',
-        comment: ''
-    },
-    firebase: {
-        members: db.ref('members')
-    },
-    methods: {
-        addItem: function() {
-            this.$firebaseRefs.members.push({
-                name: this.name,
-                imageUrl: this.imageUrl,
-                comment: this.comment
-            })
-        }
+  el: '#member-insert',
+  data: {
+    name: '',
+    imageUrl: '',
+    comment: ''
+  },
+  firebase: {
+    members: db.ref('members')
+  },
+  methods: {
+    addItem: function() {
+      this.$firebaseRefs.members.push({
+        name: this.name,
+        imageUrl: this.imageUrl,
+        comment: this.comment
+      })
     }
+  }
 })
